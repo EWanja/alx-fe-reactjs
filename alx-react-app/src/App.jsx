3969c26b26bfb3b7444f7074a13b40f20f200aef
Edith Wanja <edithwanjohi254@gmail.com>
2025-08-30 15:33:12 +0300

    Add UserProfile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,17 +2,23 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 function App() {
- 
+
   return (
     <div>
       <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
-    </div>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography" />
+    </div> 
+
   )
 }
 
