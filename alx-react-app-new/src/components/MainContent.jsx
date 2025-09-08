92a8b02ca9236edd71e4ff17656f3c2584918c9c
Edith Wanja <edithwanjohi254@gmail.com>
2025-09-08 15:58:23 +0300

    Add styling to the react components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main style={{ color: 'red' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+
+
+export default MainContent;
