0dfb7a2103ace2569d13ddc069ef273dd6ff6a56
Edith Wanja <edithwanjohi254@gmail.com>
2025-09-12 18:13:41 +0300

    Add alx-react-app-props: refactor prop drilling to use context api

--- alx-react-app copy/src/components/Footer.jsx
+++ alx-react-app copy/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer style={{backgroundColor:'navy', color:'#fff'}}>
+    <footer>
       <p>© 2023 City Lovers</p>
     </footer>
   );
--- alx-react-app copy/src/components/MainContent.jsx
+++ alx-react-app copy/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MainContent() {
   return (
-    <main style={{ color: 'red' }}>
+    <main>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
