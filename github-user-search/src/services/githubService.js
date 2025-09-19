b042ee3e1bd64031c26288cd2538f627971c6674
Edith Wanja <edithwanjohi254@gmail.com>
2025-09-19 17:02:39 +0300

    Import axios on githubServices.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,13 @@
+import axios from "axios";
+
 async function fetchUserData(username) {
 
     try {
         
-        const response = await fetch(`https://api.github.com/users/${username}`)
+        const response = await axios.get (`https://api.github.com/users/${username}`)
         if (!response.ok) throw new Error("User not found")
     
-        return await response.json()
+        return response.json()
 
     } catch (error) {
             console.error("Error fetching user data:", error.message)
