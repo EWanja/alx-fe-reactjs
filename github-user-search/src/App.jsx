382f5e263c58b15c2611a0d421d0dfac9ac1c6ae
Edith Wanja <edithwanjohi254@gmail.com>
2025-09-19 11:50:51 +0300

    Intial setup for react project for github user search application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link }from "react-router-dom"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Github User Search</h1>
+      <p>Setup</p>
+    </div>
+  )
+}
+
+export default App
