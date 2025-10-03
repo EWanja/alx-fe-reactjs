b4a4deddec297802a7f48b8987fe1ddcc9e3f3db
Edith Wanja <edithwanjohi254@gmail.com>
2025-10-04 00:45:59 +0300

    Create new project: recipe-sharing-platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-blue-500 text-4xl font-bold">React</h1>
+    </>
+  )
+}
+
+export default App
